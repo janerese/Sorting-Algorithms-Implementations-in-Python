eccbbf87d2a9255dfba7eaa65f3324e4ab814b36
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-24 01:25:13 +0800

    Bubble Sort

---
+++ bubble-sort.py
@@ -0,0 +1,14 @@
+# Bubble Sort in Python
+ 
+def bubble_sort(array):
+    for i in range(len(array)-1, 0, -1):
+        for j in range(i):
+            if array[j] > array[j + 1]:
+                temp_var = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp_var
+
+array = [77, 29, 18, 6, 37, 86, 39, 50, 55, 97]
+bubble_sort(array)
+
+print(array)
