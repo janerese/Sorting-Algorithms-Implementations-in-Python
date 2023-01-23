1270c27882d47e282ee50002eb93fdea78446b1b
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-24 02:20:38 +0800

    Merge Sort

---
+++ merge-sort.py
@@ -0,0 +1,41 @@
+# Merge Sort in Python
+
+def merge_sort(array):
+    if len(array) > 1:
+        left_array = array[:len(array)//2]
+        right_array = array[len(array)//2:]
+
+        # Recursion
+        merge_sort(left_array)
+        merge_sort(right_array)
+
+        # Merge
+        i = 0
+        j = 0
+        k = 0
+
+        while i < len(left_array) and j < len(right_array):
+            if left_array[i] < right_array[j]:
+                array[k] = left_array[i]
+                i += 1
+            else:
+                array[k] = right_array[j]
+                j += 1
+            k += 1
+
+        while i < len(left_array):
+            array[k] = left_array[i]
+            i += 1
+            k += 1
+
+        while j < len(right_array):
+            array[k] = right_array[j]
+            j += 1
+            k += 1
+
+        # print(array)
+
+array = [77, 29, 18, 6, 37, 86, 39, 50, 55, 97]
+merge_sort(array)
+
+print(array)
