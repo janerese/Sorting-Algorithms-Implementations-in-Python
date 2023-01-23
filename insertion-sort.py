f8e42f43de31e7422989970f645720db1a8c6890
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-24 02:24:13 +0800

    Update

--- insertion-sort.py
+++ insertion-sort.py
@@ -2,12 +2,13 @@
 
 def insertion_sort(array):
     for i in range(1, len(array)):
-        anchor = array[i]
-        j = i - 1
-        while j >= 0 and anchor < array[j]:
-            array[j + 1] = array[j]
-            j = j - 1
-        array[j + 1] = anchor
+        j = i
+        while array[j - 1] > array[j] and j > 0:
+            array[j - 1], array[j] = array[j], array [j - 1]
+            j -= 1
+        
+        # print(array)
+        # Code above can be used to observe every iteration
 
 array = [2, 6, 5, 1, 3, 4]
 insertion_sort(array)
