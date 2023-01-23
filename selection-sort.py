fca81f1657f115b3c18bd6d42e1888a108765353
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-24 01:04:23 +0800

    Substitute assigned values for array

--- selection-sort.py
+++ selection-sort.py
@@ -1,9 +1,9 @@
 # Selection Sort using Python
 
 def selection_sort(array):
-    for i in range(5):
+    for i in range(9):
         min_position = i
-        for j in range(i, 6):
+        for j in range(i, 10):
             if array[j] < array [min_position]:
                 min_position = j
 
@@ -12,8 +12,8 @@ def selection_sort(array):
         array[min_position] = temporary
 
         print(array)
-        
-array = [5, 3, 8, 6, 7, 2]
+
+array = [77, 29, 18, 6, 37, 86, 39, 50, 55, 97]
 selection_sort(array)
 
 print(array)
