2453261573e11bcf322e8675bcc977ebf4d176c1
Erika Jane Reyes <erikajanetreyes@iskolarngbayan.pup.edu.ph>
2023-01-24 01:02:27 +0800

    Finished Selection Sort

--- selection-sort.py
+++ selection-sort.py
@@ -1,1 +1,19 @@
 # Selection Sort using Python
+
+def selection_sort(array):
+    for i in range(5):
+        min_position = i
+        for j in range(i, 6):
+            if array[j] < array [min_position]:
+                min_position = j
+
+        temporary = array[i]
+        array[i] = array[min_position]
+        array[min_position] = temporary
+
+        print(array)
+        
+array = [5, 3, 8, 6, 7, 2]
+selection_sort(array)
+
+print(array)
